0ca15aea61353ad3588f22a9b54a9a310728d42d
MokkeMeguru <meguru.mokke@gmail.com>
2021-04-23 06:34:18 +0900

    add lsd

--- etc/init/arch/init.sh
+++ etc/init/arch/init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo pacman -S git curl vim emacs python3
+sudo pacman -S git curl vim emacs python3 lsd
--- etc/init/osx/init.sh
+++ etc/init/osx/init.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-brew install git curl vim python3
+brew install git curl vim python3 lsd
 brew cask install emacs
